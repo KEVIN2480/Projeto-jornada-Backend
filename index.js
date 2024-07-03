fc03013d22d174e5ca49fa3e4146a8ba932c14d7
KEVIN2480 <162490652+KEVIN2480@users.noreply.github.com>
2024-07-03 12:35:26 +0000

    get read all

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get("/oi", function (req,res)) {
+app.get("/oi", function (req,res){
     res.send('Olá,mundo!')
-}
+})
 app.listen(3000)
+
+
+const lista = ['Rick Sanches', 'Morty Smith','Summer Smith']
+app.get("/item", function (req,res){
+  res.send(lista)
+})
