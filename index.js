01004bf9f138a7e7503f05d5a036d41808e03115
KEVIN2480 <162490652+KEVIN2480@users.noreply.github.com>
2024-07-03 12:33:46 +0000

    get read all

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
-const express = require('express')
-const app = express()
-
-app.get('/oi', function (req, res) {
-  res.send('Olá, mundo!')
-})
-
+app.get("/oi", function (req,res)) {
+    res.send('Olá,mundo!')
+}
 app.listen(3000)
